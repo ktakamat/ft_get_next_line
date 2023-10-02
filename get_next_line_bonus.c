8308aeb7aa2e3c50b5f682dabb9986d03c7436de
高松浩大 <machi@takamatsuhiroshidainoMacBook-Air.local>
2023-10-02 17:11:42 +0900

    add all

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,92 @@
 /*   By: machi <machi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:01:19 by machi             #+#    #+#             */
-/*   Updated: 2023/09/18 16:01:20 by machi            ###   ########.fr       */
+/*   Updated: 2023/10/02 15:30:15 by machi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line_bonus.h"
+
+char	*read_file(int fd, char *save)
+{
+	char	*buf;
+	ssize_t	result;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	while (!ft_strchr(save, '\n'))
+	{
+		result = read(fd, buf, BUFFER_SIZE);
+		if (result == -1)
+		{
+			free(buf);
+			free(save);
+			return (NULL);
+		}
+		if (result == 0)
+			break ;
+		buf[result] = '\0';
+		save = ft_strjoin(save, buf);
+	}
+	free(buf);
+	return (save);
+}
+
+char	*get_line(char *save)
+{
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	while (save[i] && save[i] != '\n')
+	{
+		i++;
+	}
+	line = (char *)malloc((sizeof(char) * (i + 2)));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, save, i + 2);
+	return (line);
+}
+
+char	*reset_save(char *save)
+{
+	char	*save_new;
+	size_t	i;
+
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (NULL);
+	}
+	save_new = (char *)malloc(sizeof(char) * (ft_strlen(save) - i));
+	if (!save_new)
+		return (NULL);
+	ft_strlcpy(save_new, &save[i + 1], ft_strlen(save) - i);
+	free(save);
+	return (save_new);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save[OPEN_MAX];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	save[fd] = read_file(fd, save[fd]);
+	if (!save[fd] || !(*save[fd]))
+	{
+		free(save[fd]);
+		return (NULL);
+	}
+	line = get_line(save[fd]);
+	if (!line)
+		return (NULL);
+	save[fd] = reset_save(save[fd]);
+	return (line);
+}
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,30 @@
 /*   By: machi <machi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:01:22 by machi             #+#    #+#             */
-/*   Updated: 2023/09/18 16:01:23 by machi            ###   ########.fr       */
+/*   Updated: 2023/10/02 15:32:47 by machi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stddef.h> 
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# define OPEN_MAX 10240
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*get_next_line(int fd);
+
+#endif
